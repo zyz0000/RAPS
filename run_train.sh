703a8a410f5b70a81a2906e38b29e0f5731045f2
Yuzhe Zhang <42911581+zyz0000@users.noreply.github.com>
2022-10-16 15:34:24 +0800

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -10,7 +10,7 @@ K=1
 CUDA_VISIBLE_DEVICES="1" python train_demo.py \
     --trainN $N --N $N --K $K --Q 1 --dot \
     --hidden_size 768 --val_step 1000 --lr $LR \
-    --pretrain_ckpt /data/zyz/practice/RE/HCRP/bert-base-uncased \
+    --pretrain_ckpt ./bert-base-uncased \
     --batch_size 2 --save_ckpt ./checkpoint/camery-ready-$N-$K.pth.tar \
     --cat_entity_rep \
     --backend_model bert
